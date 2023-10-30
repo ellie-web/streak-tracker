6dbecb332f6807d1910104d2e185e01fe1e91a4f
ellie-web <ellie.k.1711@gmail.com>
2023-10-30 22:19:55 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,54 +1,19 @@
-import { createServer } from "http";
 import { createRequestHandler } from "@remix-run/express";
-import compression from "compression";
-import express, { static as expressStatic } from "express";
-import morgan from "morgan";
+import { broadcastDevReady } from "@remix-run/node";
+import express from "express";
+
+// notice that the result of `remix build` is "just a module"
 import * as build from "../build/index.js";
 
-const MODE = process.env.NODE_ENV;
-
 const app = express();
+app.use(express.static("public"));
 
-// You need to create the HTTP server from the Express app
-const httpServer = createServer(app);
+// and your app is "just a request handler"
+app.all("*", createRequestHandler({ build }));
 
-
-app.use(compression());
-
-// You may want to be more aggressive with this caching
-app.use(expressStatic("public", { maxAge: "1h" }));
-
-// Remix fingerprints its assets so we can cache forever
-app.use(expressStatic("public/build", { immutable: true, maxAge: "1y" }));
-
-app.use(morgan("tiny"));
-app.all(
-  "*",
-  MODE === "production"
-    ? createRequestHandler({ build })
-    : (req, res, next) => {
-        purgeRequireCache();
-        return createRequestHandler({ build, mode: MODE })(req, res, next);
-      },
-);
-
-const port = process.env.PORT || 3000;
-
-// instead of running listen on the Express app, do it on the HTTP server
-httpServer.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+app.listen(3000, () => {
+  if (process.env.NODE_ENV === "development") {
+    broadcastDevReady(build);
+  }
+  console.log("App listening on http://localhost:3000");
 });
-
-////////////////////////////////////////////////////////////////////////////////
-function purgeRequireCache() {
-  // purge require cache on requests for "server side HMR" this won't let
-  // you have in-memory objects between requests in development,
-  // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, we prefer the DX of this though, so we've included it
-  // for you by default
-  // for (const key in require.cache) {
-  //   if (key.startsWith(BUILD_DIR)) {
-  //     delete require.cache[key];
-  //   }
-  // }
-}
