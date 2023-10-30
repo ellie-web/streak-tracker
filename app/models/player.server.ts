a7c05bf5302c85e9d5c57984c018b647dcef2e41
ellie-web <ellie.k.1711@gmail.com>
2023-10-30 22:29:51 +0300

    Delete unused models

--- app/models/player.server.ts
+++
@@ -1,6 +0,0 @@
-import type { Player } from "@prisma/client"
-import { prisma } from "~/db.server"
-
-export const getPlayersAll = async (): Promise<Player[]> => {
-  return await prisma.player.findMany({})
-}
